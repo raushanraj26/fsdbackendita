26964de1dd94eab2505d940ca89bdb5973c0bbd5
Raushan Raj <146066440+raushanraj26@users.noreply.github.com>
2025-02-17 12:28:11 +0530

    Delete Day1 directory

--- Day1/p1.js
+++
@@ -1,2 +0,0 @@
-const data="hello world"
-console.log(data);
--- Day1/p3.js
+++
@@ -1,6 +0,0 @@
-//here we can write the data no performing read operation
-//overwrite kr dega
-
-const fs=require('fs');
-const data="I am new data"
-fs.writeFileSync("data.txt",data);
